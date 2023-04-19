f038466885f87a1b3f26e6f35ef3a8f7270cda4e
cxxy <seregamil38@gmail.com>
2023-04-19 09:59:42 +0300

    Remove ASP worker application; Create console application like real emulation stand

---
+++ RoboNode/Program.cs
@@ -0,0 +1,12 @@
+﻿using Platform;
+
+const int port = 31253;
+
+var robot = new Robot(Guid.NewGuid(), "#botyanya", port);
+
+robot.OnProducerConnected += socket => Console.WriteLine($"Connected producer {socket}");
+robot.OnProducerDisconnected += () => Console.WriteLine("Producer disconnected");
+        
+robot.OnKeyDown += name => Console.WriteLine($"Key DOWN: {name}");
+robot.OnKeyUp += name => Console.WriteLine($"Key UP: {name}");
+robot.OnJoystickUsed += vector => Console.WriteLine($"JoyX: {vector.X}; JoyY: {vector.Y}");
