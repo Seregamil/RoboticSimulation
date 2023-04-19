9a3dfc37da196ecf67b8bce6b4409fc872afcdf4
cxxy <seregamil38@gmail.com>
2023-04-19 10:04:33 +0300

    Add serilog writer

--- RoboNode/Program.cs
+++ RoboNode/Program.cs
@@ -1,12 +1,18 @@
 ﻿using Platform;
+using Serilog;
 
 const int port = 31253;
 
+var log = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var robot = new Robot(Guid.NewGuid(), "#botyanya", port);
 
-robot.OnProducerConnected += socket => Console.WriteLine($"Connected producer {socket}");
-robot.OnProducerDisconnected += () => Console.WriteLine("Producer disconnected");
+robot.OnProducerConnected += socket => log.Information($"Connected producer {socket}");
+robot.OnProducerDisconnected += () => log.Warning("Producer disconnected");
         
-robot.OnKeyDown += name => Console.WriteLine($"Key DOWN: {name}");
-robot.OnKeyUp += name => Console.WriteLine($"Key UP: {name}");
-robot.OnJoystickUsed += vector => Console.WriteLine($"JoyX: {vector.X}; JoyY: {vector.Y}");
+robot.OnKeyDown += name => log.Debug($"Key DOWN: {name}");
+robot.OnKeyUp += name => log.Debug($"Key UP: {name}");
+robot.OnJoystickUsed += vector => log.Debug($"JoyX: {vector.X}; JoyY: {vector.Y}");
